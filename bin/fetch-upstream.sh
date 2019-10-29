3a442b9d208e8cefe895abb2f2b198cf5d9daa14
Paul Wright <pwright@redhat.com>
2019-10-29 14:46:01 +0000

    fix runtime guide and update script

--- bin/fetch-upstream.sh
+++ bin/fetch-upstream.sh
@@ -1,3 +1,4 @@
 ./bin/asciidoc-coalescer.rb  -a allow-uri-read master-remote.adoc > master.adoc
+sed -i 's/:leveloffset!:/:leveloffset: -1/g' master.adoc
 ./bin/asciidoc-coalescer.rb  -a allow-uri-read guide-cloud-openshift/README.adoc > guide-cloud-openshift/master.adoc
 ./bin/asciidoc-coalescer.rb  -a allow-uri-read guide-getting-started/README.adoc > guide-getting-started/master.adoc
