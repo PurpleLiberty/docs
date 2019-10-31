6fcb791cb82caa9b11b47662565d39604f9ad319
Nicole Baratta <nengard@redhat.com>
2019-10-31 10:34:46 -0500

    add release notes

--- bin/fetch-upstream.sh
+++ bin/fetch-upstream.sh
@@ -2,3 +2,4 @@
 sed -i 's/:leveloffset!:/:leveloffset: -1/g' master.adoc
 ./bin/asciidoc-coalescer.rb  -a allow-uri-read guide-cloud-openshift/README.adoc > guide-cloud-openshift/master.adoc
 ./bin/asciidoc-coalescer.rb  -a allow-uri-read guide-getting-started/README.adoc > guide-getting-started/master.adoc
+./bin/asciidoc-coalescer.rb  -a allow-uri-read releasenotes/master-remote.adoc > releasenotes/master.adoc
