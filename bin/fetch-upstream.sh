826bccdca04388266dcdf05e802c36bb7ab4eaa0
Nicole Baratta <nengard@redhat.com>
2020-03-12 09:05:46 -0500

    demo build

--- bin/fetch-upstream.sh
+++ bin/fetch-upstream.sh
@@ -3,3 +3,4 @@ sed -i 's/:leveloffset!:/:leveloffset: -1/g' master.adoc
 ./bin/asciidoc-coalescer.rb  -a allow-uri-read guide-cloud-openshift/README.adoc > guide-cloud-openshift/master.adoc
 ./bin/asciidoc-coalescer.rb  -a allow-uri-read guide-getting-started/README.adoc > guide-getting-started/master.adoc
 ./bin/asciidoc-coalescer.rb  -a allow-uri-read releasenotes/master-remote.adoc > releasenotes/master.adoc
+./bin/asciidoc-coalescer.rb  -a allow-uri-read demo/master-remote.adoc > demo/master.adoc
