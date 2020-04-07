debf0735657ee1bb862a9cc435926143aee0d156
Nicole Baratta <nengard@redhat.com>
2020-04-07 11:42:04 -0500

    fix runtime guide

--- bin/fetch-upstream.sh
+++ bin/fetch-upstream.sh
@@ -1,7 +1,3 @@
-./bin/asciidoc-coalescer.rb  -a allow-uri-read master-remote.adoc > master.adoc
-sed -i 's/:leveloffset!:/:leveloffset: -1/g' master.adoc
-./bin/asciidoc-coalescer.rb  -a allow-uri-read guide-cloud-openshift/README.adoc > guide-cloud-openshift/master.adoc
-./bin/asciidoc-coalescer.rb  -a allow-uri-read guide-getting-started/README.adoc > guide-getting-started/master.adoc
 ./bin/asciidoc-coalescer.rb  -a allow-uri-read releasenotes/master-remote.adoc > releasenotes/master.adoc
-./bin/asciidoc-coalescer.rb  -a allow-uri-read demo/master-remote.adoc > demo/master.adoc
 ./bin/asciidoc-coalescer.rb  -a allow-uri-read runtime-guide/master-remote.adoc > runtime-guide/master.adoc
+sed -i 's/:leveloffset!:/:leveloffset: -1/g' runtime-guide/master.adoc
