c607b5a2b7a29c9f3e96487b1c1c8cd2e79f7b8f
Austin <Austin.Bailey@ibm.com>
2020-10-06 13:59:44 +0100

    Update fetch-upstream.sh to grab images
    Signed-off-by: Austin Bailey <Austin.Bailey@ibm.com>

--- bin/fetch-upstream.sh
+++ bin/fetch-upstream.sh
@@ -1,3 +1,8 @@
 ./bin/asciidoc-coalescer.rb  -a allow-uri-read releasenotes/master-remote.adoc > releasenotes/master.adoc
 ./bin/asciidoc-coalescer.rb  -a allow-uri-read runtime-guide/master-remote.adoc > runtime-guide/master.adoc
 sed -i 's/:leveloffset!:/:leveloffset: -1/g' runtime-guide/master.adoc
+targets=$(cat releasenotes/master.adoc | grep 'image::' | cut -c8- | awk -F[ '{ print $1 }')
+for image in $targets; do
+    echo "Fetching image: '$image'"
+    curl https://raw.githubusercontent.com/OpenLiberty/blogs/prod/$image --create-dirs -o ../$image
+done
